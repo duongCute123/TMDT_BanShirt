c885989295568ab2f064389563abd4f5909d9537
Your Name <you@example.com>
2023-03-10 16:21:00 +0700

    finish login register

--- src/component/auth/login.jsx
+++ src/component/auth/login.jsx
@@ -1,7 +1,46 @@
-const Login=()=>{
-    return(
-        <div className="login">
-            <h1>Login</h1>
+import { useState } from "react"
+import axios from "axios"
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+const Login = () => {
+    const list = {
+        ussername: "",
+        password: ""
+    }
+    const [forms, setForms] = useState(list)
+    const { ussername, password } = forms
+    const layTT = (e) => {
+        const { name, value } = e.target
+        setForms({ ...forms, [name]: value })
+    }
+    const hangdSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div className="login container-fluid col-sm-6">
+            <div className="login-form">
+                <h1 style={{
+                    textAlign: "center"
+                }}>Login</h1>
+                <form action="" method="post">
+                    <div className="form-group">
+                        <label htmlFor="">Nhập tên đăng nhập</label>
+                        <input type="text" name="ussername" value={ussername} placeholder="Vui lòng nhập tên đăng nhập" onChange={layTT} className="form-control" id="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Nhập mật khẩu đăng nhập</label>
+                        <input type="text" name="password" value={password} placeholder="Vui lòng nhập mật khẩu đăng nhập" onChange={layTT} className="form-control" id="" />
+                    </div>
+                    <div className="col-sm-12" style={{
+                        textAlign:"center"
+                    }}>
+                        <input type="submit" className="btn btn-primary" value="Login" onClick={hangdSubmit} />
+                    </div>
+                </form>
+            </div>
+            <div className="login-register">
+                <p>Bạn chưa có tài khoản? <Link to={"/register"}>Đăng ký</Link></p>
+            </div>
         </div>
     )
 }
--- src/component/auth/register.jsx
+++ src/component/auth/register.jsx
@@ -1,7 +1,62 @@
-const Register=()=>{
-    return(
-        <div className="register">
-            <h1>Register</h1>
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import imagelogin from "../../image.modue/login-3305943-2757111.png"
+const Register = () => {
+    const list = {
+        fullname: "",
+        username: "",
+        password: "",
+        email: ""
+    }
+    const [forms, setForm] = useState(list)
+    const { fullname, username, password, email } = forms
+    const layTT = (e) => {
+        const { name, value } = e.target
+        setForm({ ...forms, [name]: value })
+    }
+    const hangchangSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div className="register container-fluid col-sm-6" style={{
+            alignItems:"center"
+        }}>
+            <div className="row">
+                <div className="register-form col-sm-4">
+                    <h1 style={{
+                        textAlign: "center",
+                        color: "red"
+                    }}>Register</h1>
+                    <form action="" method="post">
+                        <div className="from-group">
+                            <label htmlFor="">Nhập họ và tên của bạn</label>
+                            <input type="text" className="form-control" name="fullname" value={fullname} onChange={layTT} placeholder="Vui lòng nhập họ và tên ban" id="" />
+                        </div>
+                        <div className="from-group">
+                            <label htmlFor="">Nhập tên tài khoản</label>
+                            <input type="text" className="form-control" name="username" value={username} onChange={layTT} placeholder="Vui lòng nhập tên đăng nhập" id="" />
+                        </div>
+                        <div className="from-group">
+                            <label htmlFor="">Nhập mật khẩu đăng ký</label>
+                            <input type="text" className="form-control" name="password" value={password} placeholder="Vui lòng nhập mật khẩu đăng ký" onChange={layTT} id="" />
+                        </div>
+                        <div className="from-group">
+                            <label htmlFor="">Nhập email đăng ký</label>
+                            <input type="text" className="form-control" name="email" value={email} placeholder="Vui lòng nhập email đăng ký" onChange={layTT} id="" />
+                        </div>
+                        <div className="regiter-login">
+                            <input type="submit" value="Register" className="btn btn-primary" />
+                        </div>
+                    </form>
+                    <div className="register-lo">
+                        <p>Bạn đã có tài khoản!! <Link to={"/login"}>Login</Link></p>
+                    </div>
+                </div>
+                <div className="image-form col-sm-4">
+                    <img width={"340px"} src={imagelogin} alt="duong" />
+                </div>
+            </div>
+
         </div>
     )
 }
