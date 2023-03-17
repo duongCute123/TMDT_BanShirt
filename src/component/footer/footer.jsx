106046cd2ab3053391df9e49c4ca1d1a855ad5f9
Your Name <you@example.com>
2023-03-17 09:39:57 +0700

    add footer page

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,34 @@
+const Footer=()=>{
+    return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-3">
+                    <h1>Web bán hàng Online</h1>
+                    <p>Rất vui vì bạn ghé thăm</p>
+                </div>
+                <div className="col-sm-3">
+                    <h3>Các chức năng có trên web</h3>
+                    <ul>
+                        <li>View Product</li>
+                        <li>Many Product</li>
+                        <li>Login and Register</li>
+                        <li>ShopingCart</li>
+                        <li>Seach Product with Name Product</li>
+                    </ul>
+                </div>
+                <div className="col-sm-3">
+                    <h1>People writing:Nguyen Van Duong</h1>
+                    <h3>Thông tin liên hệ</h3>
+                    <ul>
+                        <li>SDT:0338629576</li>
+                        <li>Thông tin khác none</li>
+                    </ul>
+                </div>
+                <div className="col-sm-3">
+                    <h1>Phát triển thêm sau</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer
--- src/component/product/listproduct.jsx
+++ src/component/product/listproduct.jsx
@@ -1,36 +1,138 @@
-import { useEffect } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
 import { getprocduct } from "../action/productaction"
-
-const GetProduct = () => {
-    const product = useSelector(state => state.product.Product)
-    const dispatch = useDispatch()
+const LayShirt = () => {
+    const [data, setData] = useState([])
     useEffect(() => {
-        dispatch(getprocduct(product))
-    })
-    console.log(product);
-    const Product = ({ products: { tenSpham, anh, giaSpham, loaiSp, nhaSX, thongTin } }) => {
-        return (
-            <div className="container-fluid">
-                <div>
-                    <h1>{tenSpham}</h1>
-                </div>
-            </div>
-        )
-    }
-    const ListItem=({items})=>{
-        const listitem=items.map((list)=><Product products={list}/>)
-        console.log(listitem);
-        return listitem
-    }
+        axios.get("https://633e2bdbc235b0e5751fe7a6.mockapi.io/products")
+            .then(res => {
+                setData(res.data)
+            })
+            .then(res => {
+                console.log("Lấy dữ liệu thành công");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    console.log(data);
+    return (
+        <div className="container-fluid">
+            <h1>Ưu đãi lớn</h1>
+            {
+                data.filter(list => list.loaiSp.toLowerCase().includes("shirt")).map((list) => {
+                    return (
+                        <div className="container-fluid">
+                            <div className="list-shirt row" style={{ margin: "5px" }}>
+                                <div className="col-sm-12">
+                                    <div className="thumbnail">
+                                        <Link to={"/product/" + list.id}>
+                                            <img width={"234px"} height={"160px"} src={list.anh} alt={list.tenSpham} />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+const LayCoast = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get("https://633e2bdbc235b0e5751fe7a6.mockapi.io/products")
+            .then(res => {
+                setData(res.data)
+            })
+            .then(res => {
+                console.log("Lấy dữ liệu thành công");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    console.log(data);
+    return (
+        <div className="container-fluid">
+            <h1>Sản phẩm phổ biến</h1>
+            {
+                data.filter(list => list.loaiSp.toLowerCase().includes("coast")).map((list) => {
+                    return (
+                        <div className="container-fluid">
+                            <div className="list-shirt row" style={{ margin: "5px" }}>
+                                <div className="col-sm-12">
+                                    <div className="thumbnail">
+                                        <Link to={"/product/" + list.id}>
+                                            <img width={"234px"} height={"160px"} src={list.anh} alt={list.tenSpham} />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+const LayVay = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get("https://633e2bdbc235b0e5751fe7a6.mockapi.io/products")
+            .then(res => {
+                setData(res.data)
+            })
+            .then(res => {
+                console.log("Lấy dữ liệu thành công");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    console.log(data);
+    return (
+        <div className="container-fluid">
+            <h1>Sản phẩm yêu thích</h1>
+            {
+                data.filter(list => list.loaiSp.toLowerCase().includes("dress")).map((list) => {
+                    return (
+                        <div className="container-fluid">
+                            <div className="list-shirt row" style={{ margin: "5px" }}>
+                                <div className="col-sm-12">
+                                    <div className="thumbnail">
+                                        <Link to={"/product/" + list.id}>
+                                            <img width={"234px"} height={"160px"} src={list.anh} alt={list.tenSpham} />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+const GetProduct = () => {
+
     return (
         <div className="container-fluid">
             <div className="colum">
-                <h1>Hello ban nhe</h1>
-                <ListItem items={product}/>
+                <div className="col-sm-12">
+                    <LayShirt />
+                </div>
+                <div className="col-sm-12">
+                    <LayCoast />
+                </div>
+                <div className="col-sm-12">
+                    <LayVay />
+                </div>
             </div>
         </div>
     )
 }
-
 export default GetProduct
